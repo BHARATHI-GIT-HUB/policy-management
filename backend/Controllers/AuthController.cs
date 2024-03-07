9a8d2bcd6b0e872b764b85e8859c9fa8e6b82436
BHARATHI <sbharathi261@gmail.com>
2024-03-08 00:39:29 +0530

    added backend auth

--- backend/Controllers/AuthController.cs
+++ backend/Controllers/AuthController.cs
@@ -6,12 +6,14 @@ using System.Text.Json;
 using DocumentFormat.OpenXml.Spreadsheet;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using RepositryAssignement.BO;
 using RepositryAssignement.Models;
 using RepositryAssignement.Repository;
 using System.CodeDom;
+using System.Security.Claims;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -46,31 +48,7 @@ namespace RepositryAssignement.Controllers
 
             if (userData != null)
             {
-                
-            
-                var userInfo = new 
-                {
-                    Name = userData.Username,
-                    UserId = userData.Id,
-                    Role = userData.Role.Type,
-                };
-
-                var userInfoJson = System.Text.Json.JsonSerializer.Serialize(userInfo);
-
-                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345superSecretKey@345"));
-                var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
-                var tokenOptions = new JwtSecurityToken(
-                    issuer: "",
-                    audience: "",
-                    claims: null ,
-                    expires: DateTime.Now.AddMinutes(5),
-                    signingCredentials: signinCredentials
-               
-                );
-                
-                tokenOptions.Payload["userInfo"] = userInfoJson;
-
-                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+                    string tokenString  = CreateToken(userData);
                 return Ok(new  { Token = tokenString });
             }
             return Unauthorized();
@@ -80,18 +58,49 @@ namespace RepositryAssignement.Controllers
                 return BadRequest(new { Message = ex.Message });
             }
         }
+
+
+
+
+        private string CreateToken(User user)
+        {
+            List<System.Security.Claims.Claim> claims = new List<System.Security.Claims.Claim>
+            {
+                new System.Security.Claims.Claim(ClaimTypes.Name, user.Username),
+                new System.Security.Claims.Claim(ClaimTypes.Role, user.Role.Type),
+                new System.Security.Claims.Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
+            };
+
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345superSecretKey@345"));
+            var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: signinCredentials);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
         // GET: api/<AuthController>
         [HttpGet]
+        [Authorize(Roles = "Admin")]
+
         public IEnumerable<string> Get()
         {
+            var authHeader = HttpContext.Request.Headers["Authorization"];
+            Console.WriteLine(authHeader);
             return new string[] { "value1", "value2" };
         }
 
-        [Authorize]
-        // GET api/<AuthController>/5
         [HttpGet("{id}")]
+        [Authorize(Roles = "Admin")]
+        // GET api/<AuthController>/5
         public string Get(int id)
         {
+            var authHeader = HttpContext.Request.Headers["Authorization"];
+            Console.WriteLine(authHeader);
             return "value";
         }
 
